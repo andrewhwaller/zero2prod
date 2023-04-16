dc4729282e06feb1e66f96f8ff9fbb4d33afb7c3
andrewhwaller <andrew@andrewhwaller.com>
2023-04-16 03:19:09 -0500

    Update health check test with random db name

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,6 +2,7 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
@@ -13,7 +14,8 @@ async fn spawn_app() -> TestApp {
     let port = listener.local_addr().unwrap().port();
     let address = format!("http://127.0.0.1:{}", port);
 
-    let configuration = get_configuration().expect("Failed to read configuration.");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
+    configuration.database.database_name = Uuid::new_v4().to_string();
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to Postgres.");
