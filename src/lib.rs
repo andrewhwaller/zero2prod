69ff449ce046612063bf6e53b601c4cd0b7d5554
andrewhwaller <andrew@andrewhwaller.com>
2023-04-15 21:45:45 -0500

    Add subscriptions route

--- src/lib.rs
+++ src/lib.rs
@@ -14,9 +14,10 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
-        .listen(listener)?
-        .run();
+    .listen(listener)?
+    .run();
 
     Ok(server)
 } 
