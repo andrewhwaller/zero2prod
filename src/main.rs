4c0b8d750f3f8b1614b65ba9ab55afcfe07d8e30
andrewhwaller <andrew@andrewhwaller.com>
2023-04-16 22:48:42 -0500

    Update logger and add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 //! src/main.rs
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
@@ -6,6 +7,7 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
